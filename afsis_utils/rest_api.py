721fe63880e6e7253be50b712685b6efd6a99c69
t2 <t2@qed.ai>
2018-08-31 15:07:48 +0200

    Adding method for fetching wet chemistry

--- afsis_utils/rest_api.py
+++ afsis_utils/rest_api.py
@@ -3,9 +3,13 @@ from urllib.request import urlopen
 from pathlib import Path
 from tqdm import tqdm
 from typing import Tuple
+from pandas import DataFrame
 
-REQUEST_BASE_URL = 'https://afsisdb.qed.ai/cabinet/api/sample/' \
-                   '?group={group}&machine={machine}'
+SPECTRA_URL = 'https://afsisdb.qed.ai/cabinet/api/sample/' \
+              '?group={group}&machine={machine}'
+
+WET_CHEM_URL = 'https://afsisdb.qed.ai/cabinet/api/wetchemistry/' \
+               '?group=AfSIS'
 
 
 def urls_generator(request_url, credentials):
@@ -17,6 +21,16 @@ def urls_generator(request_url, credentials):
         r = requests.get(r['next'], auth=credentials).json()
         aws_urls = [(x['ssn'], x['binary_file']) for x in r['results']]
         for url in aws_urls:
+            yield url
+
+
+def wet_chem_generator(request_url, credentials):
+    r = requests.get(request_url, auth=credentials).json()
+    for url in r['results']:
+        yield url
+    while ('next' in r.keys()) and r['next']:
+        r = requests.get(r['next'], auth=credentials).json()
+        for url in r['results']:
             yield url
 
 
@@ -35,9 +49,19 @@ def get_spectra(group: str,
                 machine: str,
                 out_path: str,
                 credentials: Tuple[str, str],
-                base_url: str=REQUEST_BASE_URL):
+                base_url: str=SPECTRA_URL):
     out_path = Path(out_path)
     request_url = base_url.format(group=group, machine=machine)
     for ssn, url in tqdm(urls_generator(request_url, credentials)):
         data = fetch_from_bucket(url)
         save_file(out_path, ssn, data)
+
+
+def get_wet_chemistry(group: str,
+                      credentials: Tuple[str, str],
+                      base_url: str=WET_CHEM_URL):
+    request_url = base_url.format(group=group)
+    samples = []
+    for wet_chem in tqdm(wet_chem_generator(request_url, credentials)):
+        samples.append(wet_chem)
+    return DataFrame(samples)
