f8e6eb723ff61b6ff74055ce281b73e49c06328b
t2 <t2@qed.ai>
2018-08-31 15:09:54 +0200

    exposing wet chemistry function

--- afsis_utils/__init__.py
+++ afsis_utils/__init__.py
@@ -1,4 +1,4 @@
-from rest_api import get_spectra
+from rest_api import get_spectra, get_wet_chemistry
 from spectra_loading import parse_many_spectra
 
-__all__ = ['get_spectra', 'parse_many_spectra']
+__all__ = ['get_spectra', 'parse_many_spectra', 'get_wet_chemistry']
